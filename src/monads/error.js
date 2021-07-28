0cbd56785fca9d5b6f1f1cedc5b9a72f10de43e5
neeleshairalo <neelesh@airalo.com>
2021-07-27 23:25:48 -0700

    fixed error msg format

--- src/monads/error.js
+++ src/monads/error.js
@@ -2,35 +2,37 @@
 const { lmap } = require('lesscode-fp')
 
 const formatTitle = err => {
-    if(err) return 'Bad request'
+    if (err) return 'Bad request'
 }
 
 const formatErrors = err => {
     console.log(err)
-    if( err ){
+    if (err) {
         const formatError = err => {
             const name = err.path.split('.')[2]
-            if(err.errorCode && err.errorCode.startsWith('pattern')){
+            if (err.errorCode && err.errorCode.startsWith('pattern')) {
                 return `'${name}' format is invalid`
-            }else if( err.errorCode && err.errorCode.startsWith('minLength')){
+            } else if (err.errorCode && err.errorCode.startsWith('minLength')) {
                 return `'${name}' ${err.message}`
-            }else if(err.errorCode && err.errorCode.startsWith('required')){
+            } else if (err.errorCode && err.errorCode.startsWith('required')) {
                 return `'${name}' is required`
-            }else if(err.errorCode && err.errorCode.startsWith('format')){
+            } else if (err.errorCode && err.errorCode.startsWith('format')) {
                 return `'${name}' format is invalid`
-            }else if(err.errorCode && err.errorCode.startsWith('readOnly')){
+            } else if (err.errorCode && err.errorCode.startsWith('readOnly')) {
                 return `'${name}' is read-only`
-            }else if(err.errorCode && err.errorCode.startsWith('enum')){
+            } else if (err.errorCode && err.errorCode.startsWith('enum')) {
                 return `'${name}' ${err.message}`
-            }else if(err.message && err.message.startsWith('unsupported media type')){
+            } else if (err.errorCode && err.errorCode.startsWith('type')) {
+                return `${err.message}  ${err.message}`
+            } else if (err.message && err.message.startsWith('unsupported media type')) {
+                return `${err.message} `
+            } else if (err.message && err.message.startsWith('Authorization header required')) {
                 return `${err.message}`
-            }else if(err.message && err.message.startsWith('Authorization header required')){
-                return `${err.message}`
-            }else{
+            } else {
                 return err.message
             }
 
-            
+
         }
         return lmap(formatError)(err)
     }
