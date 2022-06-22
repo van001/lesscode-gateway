4545e00bd7036fea412600067c51ee96e2986e87
neeleshhenkel <neelesh@albeertinvent.com>
2022-06-22 01:32:15 -0700

    fixed operatioid

--- src/monads/middlewares.js
+++ src/monads/middlewares.js
@@ -175,7 +175,7 @@ module.exports = {
         req['Logger'] = {
             Info: async (msg) => $M(JSON.parse, Print)(JSON.stringify({ type: 'info', uuid: req.uuid, name: process.env.NAME, method: req.method, url: req.path, operationId: req.operationid, msg: msg, ts: Date.now() })),
             Warning: async (msg) => $M(JSON.parse, Print)(JSON.stringify({ type: 'warning', uuid: req.uuid, name: process.env.NAME, method: req.method, url: req.path, operationId: req.operationid, msg: msg, ts: Date.now() })),
-            Error: async (err) => await $M(JSON.parse, Print)(JSON.stringify({ type: 'error', uuid: req.uuid, name: process.env.NAME, method: req.method, url: req.path, operationId: req.operationId, status: err.status, user: req.User, title: err.title, category: err.category, errors: err.errors, trace: err.trace, ts: Date.now() }))
+            Error: async (err) => await $M(JSON.parse, Print)(JSON.stringify({ type: 'error', uuid: req.uuid, name: process.env.NAME, method: req.method, url: req.path, operationId: req.operationid, status: err.status, user: req.User, title: err.title, category: err.category, errors: err.errors, trace: err.trace, ts: Date.now() }))
         }
         //console.log = req['Logger'].Info
         next()
