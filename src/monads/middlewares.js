bc86d4a8eb0263a91668505a6544f733a2e107fc
neeleshhenkel <neelesh@albeertinvent.com>
2023-04-28 10:50:15 -0700

    adding class to activity

--- src/monads/middlewares.js
+++ src/monads/middlewares.js
@@ -112,7 +112,7 @@ module.exports = {
                         partition: (req.JWT) ? req.JWT.partition : null,
                         user: req.User,
                         id: extractId(req)(data)('id') || extractId(req)(data)('albertId'),
-                        //class : req.ACL ? req.ACL.class : null,
+                        class : req.ACL ? req.ACL.inheritedClass || req.ACL.class : null,
                         parentId: extractId(req)(data)('parentId'),
                         data: (index != -1) ? req.body[index] : req.body || {},
                         expiresAt: extractId(req)(data)('x-albert-expires') ? Date.now() + 3600000 : null
