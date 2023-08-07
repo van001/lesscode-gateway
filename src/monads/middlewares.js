725deb13baa2871be51004a0fe177627bffcf231
neeleshhenkel <neelesh@albeertinvent.com>
2023-08-07 12:22:35 +0200

    adding back the activity loggin for service to servce calls on post/update/delete

--- src/monads/middlewares.js
+++ src/monads/middlewares.js
@@ -96,7 +96,7 @@ module.exports = {
         
 
         const printActivity = req => lst => index => data => {
-            if ((req.headers.uuid != undefined || null )){
+            if ((req.headers.uuid != undefined || null ) && req.method == 'GET'){
                 return
             } else if (!req.path.endsWith('health') && res.statusCode < 300) {
                 Print(JSON.stringify(
